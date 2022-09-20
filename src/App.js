0b60ae0fa0ed70406515204ced4ed8ff70853d8a
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2022-09-20 22:38:40 +0300

    Preloader removed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Preloader />
+        {/*<Preloader />*/}
         <ScrollTop />
         <div className="content">
           <AnimatedRoutes />
